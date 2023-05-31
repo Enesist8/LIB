ba5310804d4fd94e8bc0ec33c839c278448dddc8
GeeksforGeeks <mazmmmdmnb@gmail.com>
2023-06-01 00:36:33 +0300

    LIB

---
+++ USTALYAAA/Class1.cs
@@ -0,0 +1,23 @@
+﻿using Newtonsoft.Json;
+using System.Xml.Serialization;
+
+namespace USTALYAAA
+{
+    public class Class1
+    {
+        public void S(string text)
+        {
+            string json = JsonConvert.SerializeObject(text);
+            File.WriteAllText("C:\\Users\\mazmm\\JsonPract.json", json);
+
+        }
+        public string D()
+        {
+            string text = File.ReadAllText("C:\\Users\\mazmm\\JsonPract.json");
+            string jstext = JsonConvert.DeserializeObject<string>(text);
+            return jstext;
+
+        }
+
+    }
+}
---
+++ biblio/MainWindow.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System.Windows;
+using Newtonsoft.Json;
+using USTALYAAA;
+using UstalTheme;
+using System;
+
+namespace biblio
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+            Class1 class1 = new Class1();
+            public MainWindow()
+            {
+                var d  = class1.D();
+                InitializeComponent();
+                vigruzka.Text = d;
+            }
+
+            private void s_Click(object sender, RoutedEventArgs e)
+            {
+                string s = vvod.Text;
+                class1.S(s);
+                Close();
+            }
+
+        private void But1_Click(object sender, RoutedEventArgs e)
+        {
+
+            App.Theme = "Dictionary1";
+       
+        }
+
+        private void But2_Click(object sender, RoutedEventArgs e)
+        {
+            App.Theme = "Dictionary2";
+        }
+    }
+    }
+
+/*var uri = new Uri("Dictionary1.xaml", UriKind.Relative);
+ResourceDictionary resourceDictionary = Application.LoadComponent(uri) as ResourceDictionary;
+Application.Current.Resources.Clear();
+Application.Current.Resources.MergedDictionaries.Add(resourceDictionary);*/
